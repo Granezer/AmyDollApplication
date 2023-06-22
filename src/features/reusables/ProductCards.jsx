fa67e7c60bfc8df15c92dd7548217d34235b64e4
kindluv <onyeukwuamara@gmail.com>
2023-06-22 18:59:07 +0100

    ..

---
+++ src/features/reusables/ProductCards.jsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+const ProductCards = () => {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <p>Blotin Vitafusion</p>
+            <p>$25</p>
+            <button>Add to cart</button>
+            <button>Buy Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCards
