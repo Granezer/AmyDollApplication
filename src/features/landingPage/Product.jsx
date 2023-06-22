a49c0f7caf31b6d5501e08fb589481440261a88e
kindyluv <onyeukwuamara@gmail.com>
2023-06-22 17:37:22 +0100

    ..

--- src/features/landingPage/Product.jsx
+++ src/features/landingPage/Product.jsx
@@ -21,14 +21,14 @@ const Product = () => {
             </Grid>
             <Grid item lg={12} sm={12} xs={12} xl={12} md={12} className={style.PdtT}>
                 <Grid container backgroundColor='white' boxShadow='5px 2px 5px 2px #a19797'>
-                    <Grid item lg={8} sm={8} xs={8} xl={8} md={8} backgroundColor={'rgb(229, 175, 135)'} sx={{ display: 'flex', alignItems: 'end', justifyContent: 'center' }}>
+                    <Grid item lg={8} sm={7} xs={7} xl={8} md={7} backgroundColor={'rgb(229, 175, 135)'} sx={{ display: 'flex', alignItems: 'end', justifyContent: 'center' }}>
                         <img src={ThreeGirl} style={{ width: isMobile ? '80%' : '' }} alt="" />
                     </Grid>
-                    <Grid item lg={4} sm={4} xs={4} xl={4} md={4} sx={{ pl: '5px', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-                        <Typography sx={{ p: '0px 2px' , mb: '10px', fontSize: !isMobile ? '30px' : '16px', color: !isMobile ? 'rgb(228, 132, 76) !important' : '#cd6444 !important', fontWeight: '700' }}>
+                    <Grid item lg={4} sm={5} xs={5} xl={4} md={5} sx={{ pl: '5px', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }} p={isMobile ? '10px' : ''}>
+                        <Typography sx={{ p: '0px 2px' , mb: '10px', fontSize: !isMobile ? '30px' : '14px', color: !isMobile ? 'rgb(228, 132, 76) !important' : '#cd6444 !important', fontWeight: '700' }}>
                             To see more Products for different skin tones
                         </Typography>
-                        <Button sx={{ borderRadius: '16px', color: 'white', backgroundColor: !isMobile ? 'rgb(228, 132, 76) !important' : '#cd6444 !important', fontSize: !isMobile ? '16px' : '12px', p: '0px 20px', fontWeight: '700' }}>Click Here</Button>
+                        <Button sx={{ borderRadius: '16px', color: 'white', backgroundColor: !isMobile ? 'rgb(228, 132, 76) !important' : '#cd6444 !important', fontSize: !isMobile ? '16px' : '12px', p: !isMobile ? '0px 20px' : '', fontWeight: '700' }}>Click Here</Button>
                     </Grid>
                 </Grid>
             </Grid>
