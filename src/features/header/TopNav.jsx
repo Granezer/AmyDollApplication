6dff19474a24d7829482ca72536ee48d63cf729a
Michael Onoh <onohkmichael@gmail.com>
2023-07-20 19:57:20 +0100

    Update TopNav.jsx cart length

--- src/features/header/TopNav.jsx
+++ src/features/header/TopNav.jsx
@@ -166,7 +166,7 @@ const TopNav = () => {
                   {loading ? (
                     <Skeleton variant="circular" width={40} height={40} />
                   ):(
-                  cartItems.length > 0 && (
+                  getCartItemCount() > 0 && (
                     <span
                       style={{
                         color: 'black',
@@ -306,7 +306,7 @@ const TopNav = () => {
                   {loading ? (
                     <Skeleton variant="circular" width={40} height={40} />
                   ):(
-                  cartItems.length > 0 && (
+                  getCartItemCount() > 0 && (
                     <span
                       style={{
                         color: 'black',
